15c66f827fdabaa82ddb7299f551f7a6cdc97e06
S. Praveen Kumar <109852619+SPraveenKumar-spk@users.noreply.github.com>
2024-06-10 00:31:45 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -1,28 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require("cors")
+
 const app = express();
 
 const URI = process.env.MongoDBURI;
-const connectDB = async()=>{
-    try{
-        await mongoose.connect(URI);
-        console.log("Connected to DataBase Successfully");
-    }catch(error){
-        console.log(error)
-        process.exit(0);
-    }
-}
-app.use(cors())
-app.use(express.json())
-const router = require("./auth/auth-router");
+const connectDB = async () => {
+  try {
+    await mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("Connected to DataBase Successfully");
+  } catch (error) {
+    console.log(error);
+    process.exit(0);
+  }
+};
 
+const corsOptions = {
+  origin: "https://classsync.vercel.app/",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, 
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+const router = require("./auth/auth-router");
 app.use("/api/auth/", router);
+
 const port = 5000;
 
-connectDB().then(()=>{
-app.listen(port,()=>{
-    console.log(`app listening on port : ${port}`);
-})
-})
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log(`app listening on port: ${port}`);
+  });
+});
