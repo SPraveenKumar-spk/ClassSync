927f68170d1d4a7a26510bbb8016f2d55f325d20
S. Praveen Kumar <109852619+SPraveenKumar-spk@users.noreply.github.com>
2024-08-28 00:28:38 +0530

    Update auth-controller.js

--- Server/controller/auth-controller.js
+++ Server/controller/auth-controller.js
@@ -145,7 +145,7 @@ const forgotpassword = async (req, res) => {
       expiresAt,
       used: false,
     });
-    const resetLink = `http://localhost:5173/resetpassword?token=${token}`;
+    const resetLink = `https://classsync-sooty.vercel.app/resetpassword?token=${token}`;
 
     await transporter.sendMail({
       to: email,
